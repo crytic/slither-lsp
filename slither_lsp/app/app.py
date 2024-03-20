275487d92404ffc0b54066e018ebac55f01dbf18
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2024-03-20 14:05:49 +0100

    Improve some request handlers

--- slither_lsp/app/app.py
+++ slither_lsp/app/app.py
@@ -2,6 +2,7 @@ from importlib.metadata import version as pkg_version
 from logging import Logger
 
 import slither_lsp.app.request_handlers as request_handlers
+import slither_lsp.app.types.params as slsp
 from slither_lsp.app.slither_server import SlitherServer
 
 
@@ -9,13 +10,13 @@ def create_server(logger: Logger):
     version = f"v{pkg_version('slither-lsp')}"
     server = SlitherServer(logger.getChild("server"), "slither-lsp", version)
 
-    server.feature("$/slither/getDetectorList")(request_handlers.get_detector_list)
-    server.feature("$/slither/getVersion")(request_handlers.get_version)
+    server.feature(slsp.SLITHER_GET_DETECTOR_LIST)(request_handlers.get_detector_list)
+    server.feature(slsp.SLITHER_GET_VERSION)(request_handlers.get_version)
 
-    server.feature("$/cryticCompile/solcStandardJson/autogenerate")(
+    server.feature(slsp.CRYTIC_COMPILE_SOLC_STANDARD_JSON_AUTOGENERATE)(
         request_handlers.autogenerate_standard_json
     )
-    server.feature("$/cryticCompile/getCommandLineArguments")(
+    server.feature(slsp.CRYTIC_COMPILE_GET_COMMAND_LINE_ARGUMENTS)(
         request_handlers.get_command_line_args
     )
 

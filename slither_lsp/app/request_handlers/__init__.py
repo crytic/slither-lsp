b3013f98132652356cfb9a1fbfb054ac24245694
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2024-04-01 15:32:15 +0200

    Extract type hierarchy to separate module

--- slither_lsp/app/request_handlers/__init__.py
+++ slither_lsp/app/request_handlers/__init__.py
@@ -10,3 +10,8 @@ from .goto_def_impl_refs import (
     register_on_goto_definition,
     register_on_goto_implementation,
 )
+from .type_hierarchy import (
+    register_on_get_subtypes,
+    register_on_get_supertypes,
+    register_on_prepare_type_hierarchy,
+)

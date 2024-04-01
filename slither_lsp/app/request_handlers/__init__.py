baee3575b4b0763b4b8c1270cf57b33db56a3de2
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2024-04-01 15:05:37 +0200

    Extract goto-def/impl/ref to separate module

--- slither_lsp/app/request_handlers/__init__.py
+++ slither_lsp/app/request_handlers/__init__.py
@@ -1,2 +1,7 @@
 from .analysis import *
 from .compilation import *
+from .goto_def_impl_refs import (
+    register_on_find_references,
+    register_on_goto_definition,
+    register_on_goto_implementation,
+)

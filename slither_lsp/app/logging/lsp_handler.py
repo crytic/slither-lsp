a0fde7a06b1f0a9b33caeda014d7ded672eb46f3
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2024-04-02 12:20:16 +0200

    Forward log messages to clients

---
+++ slither_lsp/app/logging/lsp_handler.py
@@ -0,0 +1,26 @@
+from logging import Handler, LogRecord, FATAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+from pygls.server import LanguageServer
+from lsprotocol.types import MessageType
+
+_level_to_type = {
+    FATAL: MessageType.Error,
+    ERROR: MessageType.Error,
+    WARNING: MessageType.Warning,
+    INFO: MessageType.Info,
+    DEBUG: MessageType.Debug,
+    NOTSET: MessageType.Debug,
+}
+
+
+class LSPHandler(Handler):
+    """
+    Forwards log messages to the LSP client
+    """
+    def __init__(self, server: LanguageServer):
+        Handler.__init__(self)
+        self.server = server
+
+    def emit(self, record: LogRecord):
+        msg = self.format(record)
+        msg_type = _level_to_type[record.levelno]
+        self.server.show_message_log(msg, msg_type=msg_type)
